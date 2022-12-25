01e3bfc70457277ae7ba046dbf114892ae4b6015
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2022-12-25 08:59:53 -0800

    working menu

--- driver.cpp
+++ driver.cpp
@@ -1,7 +1,26 @@
 #include "testers.h"
-
+void menu(){
+ cout << "WELCOME TO ====ALGORITHMS==== " <<endl;
+ cout << "WHICH PROGRAM DO YOU NEED TO RUN?" <<endl;
+ cout << "1) STRING SEARCH" <<endl;
+ cout << "2) MAX PROFIT" <<endl;
+}
 int main(){
  tester helper=tester();
-helper.stringsearch();
-helper.maxprofit();
+menu();
+ int input;
+ cin >> input;
+ if(!cin.fail()){
+ switch(input){
+  case 1:{
+  helper.stringsearch();
+  }
+  case 2:{
+  helper.maxprofit();
+  }
+ default: cout << "INVALID INPUT... " <<endl;
+ return main();
+ }
+ }
+
 }
